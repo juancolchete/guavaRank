f16289ae7f16e21f31f6ea4a45206e2f6af0d371
juancolchete <juanudkp@gmail.com>
2026-03-20 14:15:22 -0300

    ci: genOSMData pass OSM Filter

--- genOSMData.sh
+++ genOSMData.sh
@@ -2,6 +2,6 @@ touch merchant.md
 curl --location \
 'https://overpass-api.de/api/interpreter' \
 --header 'Content-Type:text/plain' \
---data '[out:json][timeout:180];area(3600059470)->.brazil;(node[${{ vars.OSM_FILTER }}](area.brazil););out body;>;out skel qt;' > merchant.json  
+--data '[out:json][timeout:180];area(3600059470)->.brazil;(node[$1](area.brazil););out body;>;out skel qt;' > merchant.json  
 jq -r '.elements[] | "## \(.tags.name // "Unnamed Location")\n**Type:** \(.type)\n**ID:** \(.id)\n**Coordinates:** \(.lat), \(.lon)\n" + ([.tags["addr:street"], .tags["addr:housenumber"], .tags["addr:suburb"], .tags["addr:city"], .tags["addr:postcode"]] | map(select(. != null and . != "")) | join(", ") | if . != "" then "**Address:** " + . + "\n" else "" end) + "**Details:**\n" + (.tags | to_entries | map("- **\(.key):** \(.value)") | join("\n"))  + "\n\n**OpenStreetMap:** [View on OSM](https://www.openstreetmap.org/node/\(.id)) \n"' merchant.json > merchant.md
 
